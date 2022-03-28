754e3fdefbd72cd936f83f18012e5f576beb97e1
Famrid <lemasson.steven@gmail.com>
2022-03-28 12:36:06 +0200

    Create arrow function mixedMessage it works

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const firstData = ['karma', 'étoile', 'chi', 'énergie', 'tantra', 'mana', 'coe
 const secondData = ['Thanos', 'Mars', 'Jupiter', 'Saturne', 'du cassoulet', 'du camembert', 'Neptune', 'Pluton'];
 const thirdData = ['manger des raviolis', 'vous faire un bain de pied', 'd\'éviter les plats épicés', 'de faire 30 minutes de marche par jour', 'de binge watch l\'intégrale du seigneur des anneaux', 'd\'éviter de croiser un verseau'];
 
+const mixedMessage = (array1, array2, array3) => {
+    return `Votre ${firstData[Math.floor(Math.random() * array1.length)]} est aligné avec ${secondData[Math.floor(Math.random() * array2.length)]}. Je vous conseille de ${thirdData[Math.floor(Math.random() * array3.length)]}.`
+};
+
+console.log(mixedMessage(firstData, secondData, thirdData));
