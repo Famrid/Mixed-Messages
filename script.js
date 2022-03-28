f9f89aa7e44311c1c490af245a9b450d03a1ddde
Famrid <lemasson.steven@gmail.com>
2022-03-28 12:26:53 +0200

    Create three arrays of data

---
+++ script.js
@@ -0,0 +1,4 @@
+const firstData = ['karma', 'étoile', 'chi', 'énergie', 'tantra', 'mana', 'coeur'];
+const secondData = ['Thanos', 'Mars', 'Jupiter', 'Saturne', 'du cassoulet', 'du camembert', 'Neptune', 'Pluton'];
+const thirdData = ['manger des raviolis', 'vous faire un bain de pied', 'd\'éviter les plats épicés', 'de faire 30 minutes de marche par jour', 'de binge watch l\'intégrale du seigneur des anneaux', 'd\'éviter de croiser un verseau'];
+
